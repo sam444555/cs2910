630c008222e2f6ee00a446a95c250dcb93cebaa0
sam444555 <sjl79@pitt.edu>
2026-09-18 06:42:01 +0000

    modern link speed

--- prime/docker/startup.py
+++ prime/docker/startup.py
@@ -8,7 +8,7 @@ ip = f'172.20.0.{id+1}'
 # add 10 ms outgoing network delay (~20 ms RTT)
 # and limit outgoing bandwidth to 10 Mbps
 subprocess.run(
-    "tc qdisc add dev eth0 root netem delay 10ms rate 10mbit",
+    "tc qdisc add dev eth0 root netem delay 10ms rate 100mbit",
     shell=True,
     check=True
 )
