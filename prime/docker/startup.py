fb5f83437f86e180c171e34aa2c9c367de4d1c48
sam444555 <sjl79@pitt.edu>
2026-09-24 23:10:39 +0000

    fixed startup indent

--- prime/docker/startup.py
+++ prime/docker/startup.py
@@ -1,31 +1,39 @@
-    import sys, subprocess, time
+import sys, subprocess, time
 
-    # replica id 
-    id = int(sys.argv[1])
-    # replica ip address (ip = 172.20.0.[0 + replica id + 1])
-    ip = f'172.20.0.{id+1}'
+# replica id 
+id = int(sys.argv[1])
 
-
-    # # One way delay in milliseconds (RTT = delay_ms*2)
-    # delay_ms = 0
-    # # Link speed in megabits/sec
-    # rate_mbps = 100
-    # subprocess.run(
-    #     f"tc qdisc add dev eth0 root netem delay {delay_ms}ms rate {rate_mbps}mbit",
-    #     shell=True,
-    #     check=True
-    # )
+# replica ip address (ip = 172.20.0.[0 + replica id + 1])
+ip = f'172.20.0.{id+1}'
 
 
+# # One way delay in milliseconds (RTT = delay_ms*2)
+# delay_ms = 0
+# # Link speed in megabits/sec
+# rate_mbps = 100
+# subprocess.run(
+#     f"tc qdisc add dev eth0 root netem delay {delay_ms}ms rate {rate_mbps}mbit",
+#     shell=True,
+#     check=True
+# )
 
-    # start spines: ./spines -I <ip address> 
-    spines =subprocess.Popen(f"./spines -I {ip}", cwd='/root/cs2910/prime/spines/daemon', shell=True)
 
-    # give spines time to start
-    time.sleep(5)
+# start spines: ./spines -I <ip address> 
+spines = subprocess.Popen(
+    f"./spines -I {ip}",
+    cwd='/root/cs2910/prime/spines/daemon',
+    shell=True
+)
 
-    # start prime: ./prime -i <server id> -g <global id>
-    subprocess.Popen(f"./prime -i {id} -g {id}", cwd='/root/cs2910/prime/bin',shell=True)
+# give spines time to start
+time.sleep(5)
 
-    # wait for spines to terminate then terminate 
-    spines.communicate()
+# start prime: ./prime -i <server id> -g <global id>
+subprocess.Popen(
+    f"./prime -i {id} -g {id}",
+    cwd='/root/cs2910/prime/bin',
+    shell=True
+)
+
+# wait for spines to terminate then terminate 
+spines.communicate()
